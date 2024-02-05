f12a3d323cfe2f386dc3ada156bcde5dac55e717
k022g0069@g.neec.ac.jp <k022g0069@g.neec.ac.jp>
2024-02-06 03:15:20 +0900

    途中

--- WinApp.cpp
+++ WinApp.cpp
@@ -14,7 +14,7 @@ LRESULT WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
     // 標準のメッセージ処理を行う
     return DefWindowProc(hwnd, msg, wparam, lparam);
 }
-
+         
 void WinApp::Initialize()
 {
     
