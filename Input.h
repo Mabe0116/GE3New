b36387e5ab03ad0437a15ae5a0abc277b8178ad2
k022g0069@g.neec.ac.jp <k022g0069@g.neec.ac.jp>
2024-02-05 18:45:59 +0900

    残し

--- Input.h
+++ Input.h
@@ -18,7 +18,20 @@ public:
 
 	void Update();
 
+	//任意のボタンを押されている
+	bool PushKey(BYTE keyNumber);
+
+	//任意のボタンが押された瞬間
+	bool TriggerKey(BYTE keyNumber);
+
+	//任意のボタンが離された瞬間
+
+
 private:
+	Microsoft::WRL::ComPtr<IDirectInput8> directInput;
 	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
+
+	BYTE key[256] = {};
+	BYTE keyPre[256] = {};
 };
 
